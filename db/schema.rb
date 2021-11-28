2eef981f06745029d9cd485a7b5112fea6d700b8
cuilei5205189 <allen5205189@gmail.com>
2021-11-29 00:11:22 +0800

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_28_154231) do
+ActiveRecord::Schema.define(version: 2021_11_28_161048) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 2021_11_28_154231) do
     t.boolean "is_admin"
     t.string "address"
     t.string "name"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
