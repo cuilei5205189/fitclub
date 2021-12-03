fb6c2f491460e841ccb72c44468e746275104af5
cuilei5205189 <allen5205189@gmail.com>
2021-12-03 19:47:40 +0800

    update config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   config.hosts << /.*\..*\.gitpod\.io/
+  config.hosts << /.*\..*\.katacoda\.com/
 
   # Do not eager load code on boot.
   config.eager_load = false
